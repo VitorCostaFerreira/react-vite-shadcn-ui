f15df5dd84f63d92e438eacd8030bc4e5a81c35b
Vitor Henrique <sootheCF@gmail.com>
2024-11-08 02:42:32 -0300

    Navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import '@/styles/globals.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from './routes/root';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "devices",
+        element: <h1>TEST</h1>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
